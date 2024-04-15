5a24fb385ddcdaea4d2377c8d6ce687e6f349914
Marcus Castro <51335230+OMarcusCastro@users.noreply.github.com>
2024-04-14 23:54:12 -0300

    add

--- scrapping_v2.py
+++ scrapping_v2.py
@@ -49,7 +49,7 @@ def get_last_page_number(pagination_list):
 def scrapping(link, price_limit):
     # link = "https://www.olx.com.br/imoveis/aluguel/estado-ce/fortaleza-e-regiao/fortaleza?bas=1&ps=1000&q=apartamento&ros=1&sd=2695&sd=2726&sd=2715&sd=2740&sd=2708&sd=2744&sd=2690&sd=2705&sd=2724&sd=2755&sd=2725&sd=2747&sd=2752&sd=2692&sd=2679&sd=2681&ss=1&o=1"
     # link = "https://www.olx.com.br/imoveis/aluguel/estado-ce/fortaleza-e-regiao/fortaleza?bas=1&pe=3500&ps=1000&q=apartamento&ros=1&sd=2695&sd=2726&sd=2715&sd=2740&sd=2708&sd=2744&sd=2690&sd=2705&sd=2724&sd=2755&sd=2725&sd=2747&sd=2752&sd=2692&sd=2679&sd=2681&ss=1"
-    driver = create_undetected_driver(headless=False)
+    driver = create_undetected_driver(headless=True)
     driver.get(link)
     # espera_elemento_presente(By.ID, "listing-pagination", driver, 10)
     pagination_list = driver.find_element(
